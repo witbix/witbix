21b74080c7053503ebe46ee7aa12ceed44f1d5fc
dbjpanda <dbjpanda@live.com>
2018-10-15 20:35:03 +0530

    Testing github token issue on travis

--- travis/drupal-build.sh
+++ travis/drupal-build.sh
@@ -2,11 +2,14 @@
 
 set +ex
 
+cat /root/.composer/auth.json
+
 docker network create -d bridge traefik-network
 docker run -d --network=traefik-network -p 80:80 -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock --name=traefik traefik:latest --api --docker
 cp .env.example .env
-GITHUB_TOKEN=$GITHUB_TOKEN docker-compose up -d
+GITHUB_TOKEN=${GITHUB_TOKEN} docker-compose up -d
 docker exec -it test composer install
+docker exec -it test cat /root/.composer/auth.json
 docker exec -it test drush si --yes
 
 if [ $(docker exec test drush status bootstrap | grep -c Successful) == 1 ]; then

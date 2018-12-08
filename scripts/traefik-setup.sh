92d247e2d86903c3f827fee6ad8bc464d15f3090
dbjpanda <dbjpanda@live.com>
2018-12-08 16:13:58 +0530

    Traefik fix

--- scripts/traefik-setup.sh
+++ scripts/traefik-setup.sh
@@ -22,7 +22,7 @@ if [ $(docker ps | grep -c traefik) == 0 ]; then
     if [ $1 == 'remote' ]; then
         docker run -d --network=traefik-network -p 80:80 -p 443:443 \
         -v /var/run/docker.sock:/var/run/docker.sock \
-        -v $PWD/docker/traefik:/etc/traefik \
+        -v $PWD/traefik:/etc/traefik \
         -v $PWD/secrets/ssl:/etc/ssl \
         --name=traefik traefik:latest --api --docker
     fi

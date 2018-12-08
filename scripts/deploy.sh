9df0d4691a1ea1f0ca6d59da297903b05d119d52
dbjpanda <dbjpanda@live.com>
2018-12-08 17:17:49 +0530

    ssh key add

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,10 @@
 
 set -xe
 
+# Start ssh-agent
 chmod 600 secrets/ssh/${DEPLOY_USER}_rsa
 eval $(ssh-agent)
 ssh-add secrets/ssh/${DEPLOY_USER}_rsa
+
+# ssh to server
+ssh -o StrictHostKeyChecking=no "${DEPLOY_USER}"@"${SERVER_IP}" mkdir test

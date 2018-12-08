e168179babd9221c55e608266776f4144a0b27ec
dbjpanda <dbjpanda@live.com>
2018-12-08 20:09:02 +0530

    ssh key add

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ ssh-add secrets/ssh/${DEPLOY_USER}_rsa
 
 #scp -o StrictHostKeyChecking=no -r . "${DEPLOY_USER}"@"${SERVER_IP}":~/test
 
-rsync -av --progress -e "ssh -T -c aes128-ctr -o Compression=no -x" . "${DEPLOY_USER}"@"${SERVER_IP}":~/test
+rsync -av --progress -e "ssh -T -c aes128-ctr -o Compression=no -o StrictHostKeyChecking=no -x" . "${DEPLOY_USER}"@"${SERVER_IP}":~/test
 
 # ssh to server
 #ssh -o StrictHostKeyChecking=no "${DEPLOY_USER}"@"${SERVER_IP}" docker-compose up -d

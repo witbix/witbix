2cd17a87e6b320967dd7533c223c02535c9fa23c
dbjpanda <dbjpanda@live.com>
2018-12-12 19:54:55 +0530

    Fix uiid

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -7,13 +7,6 @@ if [ ${ENVIRONMENT} == 'dev' ]; then
     gosu root sh -c "echo "zend_extension=xdebug.so" > /etc/php7/conf.d/00_xdebug.ini" 2>/dev/null || :
 fi
 
-## Change UID of 'deploy' as per host user UID
-#HOST_CURRENT_USER_ID=$(stat -c "%u" /var/www/${PROJECT_NAME})
-#if [ ${HOST_CURRENT_USER_ID} -ne 0 ]; then
-#    gosu root usermod -u ${HOST_CURRENT_USER_ID} deploy 2>/dev/null || :
-#    gosu root groupmod -g ${HOST_CURRENT_USER_ID} deploy 2>/dev/null || :
-#fi
-
 # Prepare Drupal
 #umask u=rwx,g=rx,o=
 cp -r -u /home/deploy/drupal-templates/${DRUPAL_VERSION}.x/. /var/www/${PROJECT_NAME}/ 2>/dev/null || :

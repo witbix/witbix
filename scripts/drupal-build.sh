a8d9e1d4dc3e6b47dba59566857b652ba6767b50
dbjpanda <dbjpanda@live.com>
2018-12-16 04:02:57 +0530

    Env variable modify

--- scripts/drupal-build.sh
+++ scripts/drupal-build.sh
@@ -3,8 +3,8 @@
 set -xe
 
 docker-compose up -d --build --remove-orphans --force-recreate
-docker exec -it witbix-debug composer install
-docker exec -it witbix-debug drush si --yes
+docker exec -it test composer install
+docker exec -it test drush si --yes
 
 if [ $(docker exec witbix-debug drush status bootstrap | grep -c Successful) == 1 ]; then
    echo  Drupal has been sucessfully built and tested.

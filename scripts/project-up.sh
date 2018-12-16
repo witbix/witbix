99be153859786d44b850c2ba9832a7a47709584a
dbjpanda <dbjpanda@live.com>
2018-12-17 02:58:23 +0530

    Fix travis

--- scripts/project-up.sh
+++ scripts/project-up.sh
@@ -7,3 +7,7 @@ if [ ! -f .env ]; then
     perl -lpe ' s/(.*)=(.*)/sprintf("%s=%s","$1",$ENV{$1}? $ENV{$1}:$2)/ge ' .env.example > .env
 fi
 docker-compose up -d --build --remove-orphans --force-recreate
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push witbix/nginx
+docker push witbix/php
+docker push witbix/mariadb

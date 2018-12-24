eab3943e1bb04b5edb158518596074ff0be2ce2d
dbjpanda <dbjpanda@live.com>
2018-12-24 06:04:58 +0530

    Change server from digitalocean to google cloud

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,40 +5,83 @@ set -xe
 DEPLOY_USER=${1}
 DEPLOY_SERVER=${2}
 DEPLOY_PATH=${3}
-PROJECT_NAME=${4}
 DEPLOY_ENV=${5}
 
-# Start ssh-agent
-    chmod 600 secrets/ssh/${DEPLOY_USER}_rsa
-    eval $(ssh-agent)
-    ssh-add secrets/ssh/${DEPLOY_USER}_rsa
-    ssh-keyscan ${DEPLOY_SERVER} >> ~/.ssh/known_hosts
+
+## Start ssh-agent and save the ssh session in a sock file defined in ControlPath
+#chmod 600 secrets/ssh/${DEPLOY_USER}_rsa
+#eval $(ssh-agent)
+#ssh -o ControlMaster=auto \
+#    -o ControlPath=/tmp/ssh-%r@%h:%p.sock \
+#    -o ControlPersist=60 \
+#    -o StrictHostKeyChecking=no \
+#    -i secrets/ssh/${DEPLOY_USER}_rsa \
+#    ${DEPLOY_USER}@${DEPLOY_SERVER}
+#
+#
+
+#ps -p $SSH_AGENT_PID > /dev/null || eval "$(ssh-agent -s)"
+#
+#cat > /tmp/ssh_config << 'EOF'
+#Host *
+#    StrictHostKeyChecking no
+#    ControlMaster auto
+#    ControlPath /tmp/ssh_%r@%h:%p.sock
+#    ControlPersist=60
+#EOF
+#
+#ssh -F /tmp/ssh_config B6xrvy06QdGi1C8MfKsXLZ9Fw@to2.tmate.io "mkdir test"
+#
+#trap "kill $SSH_AGENT_PID" exit
 
 
-if [ ${DEPLOY_ENV} == 'stage' ]; then
-
-    # Start uploading artifacts to server using rsync
-    rsync --info=progress2 \
-        --archive  \
-        --compress \
-        --group \
-        --include="secrets/traefik" \
-        --exclude-from="./scripts/.rsyncexclude" \
-        -e "ssh -T -c aes128-ctr -o Compression=no -x" \
-        ./  "${DEPLOY_USER}"@"${DEPLOY_SERVER}":"${DEPLOY_PATH}"/"${PROJECT_NAME}"-stage
-
-    # Run required scripts on server machine over ssh
-    ssh ${DEPLOY_USER}@${DEPLOY_SERVER} "cd ${DEPLOY_PATH}/${PROJECT_NAME}-stage \
-                                         && scripts/traefik-setup.sh remote \
-                                         && scripts/drupal-build.sh stage"
-fi
 
 
-if [ ${DEPLOY_ENV} == 'prod' ]; then
 
-    # Run required scripts on server machine over ssh
-    ssh ${DEPLOY_USER}@${DEPLOY_SERVER} "cd ${DEPLOY_PATH} \
-                                         && mv ${PROJECT_NAME}-stage ${PROJECT_NAME}-prod-${RANDOM} \
-                                         && cd ${PROJECT_NAME}-prod-* \
-                                         && scripts/drupal-build.sh prod"
-fi
+ps aux | grep -c "[s]sh-agent" > /dev/null || eval $(ssh-agent)
+cat > /tmp/ssh_config << EOF
+Host ${DEPLOY_SERVER}
+    IdentityFile ${PWD}/secrets/ssh/${DEPLOY_USER}_rsa
+    StrictHostKeyChecking no
+    ControlMaster auto
+    ControlPath /tmp/ssh_%r@%h:%p.sock
+    ControlPersist=60
+EOF
+ssh -F /tmp/ssh_config ${DEPLOY_USER}@${DEPLOY_SERVER}
+
+
+
+
+
+#
+#
+#if [ ${DEPLOY_ENV} == 'stage' ]; then
+#
+#    # Access the master directory name
+#    ssh ${DEPLOY_USER}@${DEPLOY_SERVER}
+#
+#    # Start uploading artifacts to server using rsync
+#    rsync --info=progress2 \
+#        --archive  \
+#        --compress \
+#        --group \
+#        --include="secrets/traefik" \
+#        --exclude-from="./scripts/.rsyncexclude" \
+#        -e "ssh -T -c aes128-ctr -o Compression=no -o ControlPath=/tmp/ssh-%r@%h:%p.sock -x" \
+#        ./  "${DEPLOY_USER}"@"${DEPLOY_SERVER}":"${DEPLOY_PATH}"/${PWD##*/}
+#
+#    # Run required scripts on server machine over ssh
+#    ssh ${DEPLOY_USER}@${DEPLOY_SERVER} "cd ${DEPLOY_PATH}/$(git rev-parse --short HEAD) \
+#                                         && scripts/traefik-setup.sh remote \
+#                                         && scripts/drupal-build.sh stage"
+#fi
+#
+#
+#if [ ${DEPLOY_ENV} == 'prod' ]; then
+#
+#    # Run required scripts on server machine over ssh
+#    ssh ${DEPLOY_USER}@${DEPLOY_SERVER} "cd ${DEPLOY_PATH} \
+#                                         && mv ${PROJECT_NAME}-stage ${PROJECT_NAME}-prod-${RANDOM} \
+#                                         && cd ${PROJECT_NAME}-prod-* \
+#                                         && scripts/drupal-build.sh prod"
+#fi
--- scripts/server-setup.sh
+++ scripts/server-setup.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
 
-echo "Creating a separate user who have deploy access on your server"
-read -p 'Server Address ' SERVER
-read -p 'Deploy User Name ' USER
+SSH_USER=${1}
+DEPLOY_SERVER=${2}
+DEPLOY_USER=${3}
 
-# Generate ssh key pair
-ssh-keygen -t rsa -N "" -f secrets/ssh/${USER}_rsa
-PUB_KEY=$(cat secrets/ssh/${USER}_rsa.pub)
+while [ -z ${SSH_USER} ] && [ -z ${DEPLOY_SERVER} ] && [ -z ${DEPLOY_USER} ]; do
+    echo "Creating a separate user for deployment purposes"
+    echo "###############################################################"
+    read -p 'Username who already have ssh access to the deploy server: ' SSH_USER
+    echo "###############################################################"
+    read -p 'Deploy Server Address: ' DEPLOY_SERVER
+    echo "###############################################################"
+    read -p 'Deploy User Name for CI/CD: ' DEPLOY_USER
+done
 
-ssh -t -o StrictHostKeyChecking=no root@"${SERVER}" << EOF
-adduser --disabled-password --gecos "" ${USER}
-usermod -a -G docker ${USER}
-su ${USER}
-mkdir -p /home/${USER}/.ssh
-chmod 700 /home/${USER}/.ssh
-echo "${PUB_KEY}" >> ~/.ssh/authorized_keys
+# Check if ssh credentials for deploy user has been already generated or exits in your local secrets/ssh dir
+if [ ! -f secrets/ssh/${DEPLOY_USER}_rsa.pub ] && [ ! -f secrets/ssh/${DEPLOY_USER}_rsa ]; then
+    # Generate ssh key pair
+    ssh-keygen -t rsa -N "" -f secrets/ssh/${DEPLOY_USER}_rsa
+fi
+PUB_KEY=$(cat secrets/ssh/${DEPLOY_USER}_rsa.pub)
+
+ssh -o StrictHostKeyChecking=no ${SSH_USER}@"${DEPLOY_SERVER}" << EOF
+set -xe
+id -u ${DEPLOY_USER} &>/dev/null || sudo adduser --disabled-password --gecos "" ${DEPLOY_USER}
+sudo usermod -a -G docker ${DEPLOY_USER}
+sudo mkdir -p /home/${DEPLOY_USER}/.ssh
+sudo sh -c " echo ${PUB_KEY} >> /home/${DEPLOY_USER}/.ssh/authorized_keys "
+sudo chown -R ${DEPLOY_USER} /home/${DEPLOY_USER}/.ssh
+sudo chmod 700 /home/"${DEPLOY_USER}"/.ssh
+echo "Authorized key successfully added for user ${DEPLOY_USER} "
 EOF

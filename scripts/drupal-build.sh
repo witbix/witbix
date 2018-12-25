40cfe160ad33ed2f3aa0e790b9b66fe01233d0fd
dbjpanda <dbjpanda@live.com>
2018-12-25 09:23:07 +0530

    Added old drupal

--- scripts/drupal-build.sh
+++ scripts/drupal-build.sh
@@ -30,15 +30,15 @@ if [ ${BUILD_ENV} == 'stage' ]; then
     fi
 
     docker-compose up -d
-    docker-compose exec php composer install
-    docker-compose exec php drush si --yes
+    docker-compose exec -T php composer install
+    docker-compose exec -T php drush si --yes
 
     if [ -f code/drupal/dump.sql ]; then
-        docker-compose exec php drush sql-cli < /code/drupal/dump.sql
-        docker-compose exec php drush cim --yes
+        docker-compose exec -T php drush sql-cli < /code/drupal/dump.sql
+        docker-compose exec -T php drush cim --yes
       else
-        docker-compose exec php drush cim --partial --yes
-        docker-compose exec php drush csim live_config --yes
+        docker-compose exec -T php drush cim --partial --yes
+        docker-compose exec -T php drush csim live_config --yes
     fi
 
 fi

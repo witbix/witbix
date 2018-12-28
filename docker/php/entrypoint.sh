67d3ee824bf6206bf25d88a9e4b0969365da2012
dbjpanda <dbjpanda@live.com>
2018-12-28 12:18:40 +0530

    Fix docker entrypoint

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Change UID of 'deploy' as per host user UID
-HOST_CURRENT_USER_ID=12000
+HOST_CURRENT_USER_ID=$(stat -c "%u" /var/www/${PROJECT_NAME})
 if [ ${HOST_CURRENT_USER_ID} -ne 0 ]; then
     if [ $(which gosu) ]; then
         gosu root usermod -u ${HOST_CURRENT_USER_ID} deploy && \

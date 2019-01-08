f462b4adecfbaf1aeb373a1088ebfb9ec4cd74d6
dbjpanda <dbjpanda@live.com>
2019-01-08 18:38:22 +0530

    fix

--- scripts/drupal-build.sh
+++ scripts/drupal-build.sh
@@ -19,11 +19,11 @@ if [ ${BUILD_ENV} == 'stage' ]; then
     docker-compose up -d
     docker-compose exec -T php composer install
 
-
     if [ -f code/drupal/dump.sql ]; then
 
         docker-compose exec -T mariadb mysql -u root -penter@8018573186 -e "CHANGE MASTER TO MASTER_HOST='witbix-yang.mariadb';"
-        docker-compose exec -T php drush sql-cli < code/drupal/dump.sql
+        docker-compose exec -T mariadb mysql -u root -penter@8018573186 drupal < code/drupal/dump.sql
+#        docker-compose exec -T php drush sql-cli < code/drupal/dump.sql
 #        docker-compose exec -T php drush cim --yes
       else
         docker-compose exec -T php drush si --yes
